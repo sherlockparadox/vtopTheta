bbd9cf0d9bceb348c0fc2e0557d79583a484f9c7
Kapil Shrotriya <cross399@gmail.com>
2018-04-07 09:05:07 +0530

    App.js
    Added App bar and Raised button

--- theta/src/App.js
+++ theta/src/App.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+import RaisedButton from 'material-ui/RaisedButton';
 import logo from './logo.svg';
 import './App.css';
+
+const style = {
+  margin: 12,
+};
 
 class App extends Component {
   render() {
     return (
+    <MuiThemeProvider>
+      <AppBar
+    title="Title"
+    iconClassNameRight="muidocs-icon-navigation-expand-more"
+  />
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
@@ -14,6 +27,8 @@ class App extends Component {
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
+      <RaisedButton label="Primary" primary={true} style={style} />
+    </MuiThemeProvider>
     );
   }
 }
