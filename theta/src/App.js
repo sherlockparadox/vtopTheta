f5afc4a124c998aaa0c4d1b793fbd73dc06c50a3
sherlockparadox <architsingh007@gmail.com>
2018-04-09 15:15:24 +0530

    App.js clean
    Deleted useless stuff.

--- theta/src/App.js
+++ theta/src/App.js
@@ -1,19 +1,7 @@
 import React, { Component } from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import RaisedButton from 'material-ui/RaisedButton';
-import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
-import FlatButton from 'material-ui/FlatButton';
-import Toggle from 'material-ui/Toggle';
-import logo from './logo.svg';
 import './App.css';
 import TabsExampleSwipeable from './tab_nav'
-
-
-
-
-const style = {
-  margin: 12,
-};
  
 class App extends Component {
     //Card
